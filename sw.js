0dd6f04742abeffd2635411ec23fb472e7d8c999
jmayers11 <jmayers1@liberty.edu>
2026-06-13 19:57:40 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Caches static assets + offline fallback page
    ============================================================= */
 
-const CACHE_NAME = 'church-dash-v36';
+const CACHE_NAME = 'church-dash-v37';
 const OFFLINE_URL = './offline.html';
 
 // Static assets to pre-cache on install
